28573d28108e700adb28446f7fe0b93469538f82
Gabriel D'Aquino <gabriel.leonardo.d@gmail.com>
2018-07-08 16:40:58 -0400

    Added complex fading description for sidebar items

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,30 @@ var currFile = url.substr(url.lastIndexOf('/') + 1)
 
 var sidebar = document.getElementById('sidebar')
 var currIcon = sidebar.querySelectorAll('[href="'+currFile+'"]')[0]
-currIcon.id = "active-item"
+currIcon.id = 'active-item'
+
+var allIcons = sidebar.getElementsByClassName('item')
+
+var div = []
+for (var i = 0; i < allIcons.length; ++i) {
+	div.push(document.createElement('div'))
+	var rect = allIcons[i].getBoundingClientRect()
+	div[i].style.cssText = 'position:fixed;color:#7aa5ff;background-color:#00000055;font-size:24px;right:85px;top:' + (rect.y - 3) + 'px;padding:6px;border:2px solid #383a62;opacity: 0;transition:opacity 0.3s'
+	allIcons[i].onmouseover = function() {
+		for (var i = 0; i < div.length; ++i) {
+			if (div[i].textContent == this.textContent) {
+				div[i].style.opacity = 1
+			}
+		}
+	}
+
+	allIcons[i].onmouseout = function() {
+		for (var i = 0; i < div.length; ++i) {
+			if (div[i].textContent == this.textContent) {
+				div[i].style.opacity = 0
+			}
+		}
+	}
+	div[i].textContent = allIcons[i].getElementsByClassName('menu-desc')[0].textContent
+	document.body.appendChild(div[i])
+}
