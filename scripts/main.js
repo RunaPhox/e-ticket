bc23a8f107f781880a2fb6aba4525b2276665a60
Gabriel D'Aquino <gabriel.leonardo.d@gmail.com>
2018-07-09 22:44:01 -0400

    Implemented blockchain-like log in inicio.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
+var saldo = 4132
 var url = window.location.href
 
 var currFile = url.substr(url.lastIndexOf('/') + 1)
+if (currFile === 'inicio.html') {
+	appendToUses()
+}
 
 var sidebar = document.getElementById('sidebar')
 var currIcon = sidebar.querySelectorAll('[href="'+currFile+'"]')[0]
@@ -30,4 +34,37 @@ for (var i = 0; i < allIcons.length; ++i) {
 	}
 	div[i].textContent = allIcons[i].getElementsByClassName('menu-desc')[0].textContent
 	document.getElementById('sidebar').appendChild(div[i])
+}
+
+
+function appendToUses() {
+	var row = document.createElement('tr')
+
+	var now = new Date()
+	var date = document.createElement('td')
+	date.textContent = now.getDate() + '/' + (now.getMonth() + 1) + '/' + now.getFullYear()
+	row.appendChild(date)
+
+	var time = document.createElement('td')
+	time.textContent = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds()
+	row.appendChild(time)
+	var trans = document.createElement('td')
+	trans.textContent = 'Metro'
+	row.appendChild(trans)
+	var station = document.createElement('td')
+	station.textContent = 'Ciudad Universitaria'
+	row.appendChild(station)
+
+	var cost = document.createElement('td')
+
+	var amount = Math.floor(Math.random() * 7) + 1
+	saldo -= amount
+	document.getElementById('saldo').textContent = 'Bs. S. ' + saldo
+
+	cost.textContent = amount + ' Bs. S.'
+	row.appendChild(cost)
+
+	document.getElementById('usos').insertBefore(row, document.getElementById('usos').firstChild)
+
+	setTimeout(appendToUses, Math.floor(Math.random()*4500) + 2300)
 }
